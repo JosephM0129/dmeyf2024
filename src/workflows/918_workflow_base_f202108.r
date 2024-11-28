8ffdfc326afe80ad3cfcd4768b6ab6f352145f01
Joseph Moon <jmoon@llyasoc.com>
2024-11-28 09:33:19 -0300

    Update 918_workflow_base_f202108.r

--- src/workflows/918_workflow_base_f202108.r
+++ src/workflows/918_workflow_base_f202108.r
@@ -446,7 +446,7 @@ wf_agosto_v1 <- function( pnombrewf )
   ht <- HT_tuning_base( bo_iteraciones = 40 )  # iteraciones inteligentes
 
   # Etapas finales
-  fm <- FM_final_models_lightgbm( c(ht, ts8), ranks=c(1), qsemillas=10 )
+  fm <- FM_final_models_lightgbm( c(ht, ts8), ranks=c(1), qsemillas=5)
   SC_scoring( c(fm, ts8) )
   KA_evaluate_kaggle()  # genera archivos para Kaggle
 
